245fc0ddbf6de137bc0b475c22ab062332e3b0dc
darsan012 <dsrsangautam@gmail.com>
2024-11-03 18:17:37 -0500

    added the mongodb connection string and created connectDB function

---
+++ src/database/connection.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+// creating connectDB function
+const connectDB =async () => {
+    try {
+        // mongodb connection string
+        const con = await mongoose.connect(process.env.MONGO_URI);
+        console.log(`mongodb connected ${con.connection.host}`);
+    } catch (error) {
+        console.log(err);
+        process.exit(1);
+    }
+}
+
+export default connectDB
