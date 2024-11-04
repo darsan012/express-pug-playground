165f88d81cb5168e3255973d7fa1d0deb0799dd7
darsan012 <dsrsangautam@gmail.com>
2024-11-03 19:28:26 -0500

    connectin sucessfull to the mongodb atlas

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './src/database/connection.js'
+
+
 dotenv.config();
+
 // creating the express app
 const app = express();
 app.use(express.json());
 
 // getting the port from .env file
 const port = process.env.PORT || 3000;
+
+// connect to mongodb server
+connectDB();
 
 
 app.listen(port, () => console.log('listening on port: ', port));
--- src/database/connection.js
+++ src/database/connection.js
@@ -6,10 +6,10 @@ const connectDB =async () => {
         // mongodb connection string
         const con = await mongoose.connect(process.env.MONGO_URI);
         console.log(`mongodb connected ${con.connection.host}`);
-    } catch (error) {
+    } catch (err) {
         console.log(err);
         process.exit(1);
     }
 }
 
-export default connectDB
+export default connectDB;
