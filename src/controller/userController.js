fafdb5a8f195308ffe63ee58971ef9c31b50e5a5
darsan012 <dsrsangautam@gmail.com>
2024-11-03 22:45:26 -0500

    created the user routes

---
+++ src/controller/userController.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+const router = Router();
+
+// crud operations on the user data
+router.get('/user',()=>{}); 
+router.post('/user',()=>{});
+router.put('/user/:id',()=>{});
+router.delete('/user/:id',()=>{});
